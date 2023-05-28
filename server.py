3790c61eb87dd811c52cec9b0497daf2d0377a3c
Kacper Klensporf <kacper.klensporf@gmail.com>
2023-05-28 23:36:31 +0200

    adds possiblity to delete wallet from followed_wallets endpoint, adds requirements.txt file

--- server.py
+++ server.py
@@ -173,6 +173,7 @@ def wallet_page(address):
 
     wallet_balance = eth_data_r.wallet_balance(address)
     wallet_transactions = eth_data_r.wallet_transactions(address)
+    
     return render_template('wallet_page.html', page=address, balance=wallet_balance , transactions=wallet_transactions, user=current_user, is_f=is_followed)
 
 
